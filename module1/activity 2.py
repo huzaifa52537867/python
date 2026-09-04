29ee7fb6a8dbd1060c9719248b91dc2950b88134
huzaifa52537867 <fatemathemerchant1@gmail.com>
2026-09-04 16:33:45 +0530

    useage of print

---
+++ module1/activity 2.py
@@ -0,0 +1,10 @@
+#printing a sentence
+print("hello world")
+#printing a number
+print(9)
+#printing a blank line
+print("welcome to the world \n")
+#printing multiple values
+print("hello",5)
+#PRINTING END
+print("welcome to", end="*")
