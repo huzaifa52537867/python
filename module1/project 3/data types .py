90d2d2ea41250cca2fd48cdb4400af5ce57f1308
huzaifa52537867 <fatemathemerchant1@gmail.com>
2026-09-08 17:14:09 +0530

    data types

---
+++ module1/project 3/data types .py
@@ -0,0 +1,17 @@
+name=input("enter your name")
+gadget=input("enter your favorite gadget")
+number=-9
+rating=8.5
+is_active=True
+print(name,type(name))
+print(gadget,type(gadget))
+print(number,type(number))
+print(rating,type(rating))
+print(is_active,type(is_active))
+value=str(number)
+print(value,type(value))
+variable=name[0:2]
+letter=name[-1]
+code=variable+letter
+print(variable, letter, code)
+
